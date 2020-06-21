4a2745c187dd5ba81282b4c9affd7b3bb93ad4c7
mgarcia43 <mgarcia43@ucol.mx>
2020-06-21 17:41:45 -0500

    add code

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 if('serviceWorker' in navigator){
 
-    window.addEventListener('load', function(){
-        navigator.serviceWorker.register('./sw.js',{scope: './'})
+    
+        navigator.serviceWorker.register('/sw.js')
         .then((registration) => console.log(registration.scope))
         .catch((error) => console.error(error));
-    });
 }
 
 // CHECAR LINK https://medium.com/samsung-internet-dev/pwa-series-service-workers-los-b%C3%A1sicos-de-la-experiencia-offline-14592542c738
